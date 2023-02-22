8756474b5fd25e79606230ecc00fef2c073a62b8
Christopher Perry <christopher.perry410@gmail.com>
2023-02-21 22:47:55 -0500

    Repo: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(unused_imports, unused_import_braces, dead_code)]
+//! Read: functions and structures for reading Amz reports, and serialized types
+//! from the filesystem
+//!
+//! Write: functions and structures for writing data to the filesystem.
+//!
+//! Plan: functions and structures for planning outbound items.
+pub mod read;
+pub mod write;
+pub mod plan;
