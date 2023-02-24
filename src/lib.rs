849bbe6732dbd3828b7352d237259cfe171b057a
Chris Perry <christopher.perry410@gmail.com>
2023-02-24 10:44:16 -0500

    Doc: remove stale library descriptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-//! Read: functions and structures for reading Amz reports, and serialized types
-//! from the filesystem
-//!
-//! Write: functions and structures for writing data to the filesystem.
-//!
-//! Plan: functions and structures for planning outbound items.
 pub mod read;
 pub mod write;
 pub mod plan;
