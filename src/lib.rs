152464245143c897e5ecb3d0747035c5eaafa7f2
Chris Perry <christopher.perry410@gmail.com>
2023-02-24 10:47:46 -0500

    Feat: `Tree`, related `String` alias for names
    Define an early structure of naming conventions for objects within
    outbound planning. `Tree` is a collection that begins at a `Trunk`,
    branching out into `Group`s. Each `Group` is composed of two
    auto-generated, easily remembered words.
    
    Branching arbitrary object away from their `Trunk` reasonably matches
    the actual workflow of items. The potential in the `Tree` comes from
    the generic nature of it's components. Purchase orders, removal orders,
    bundles, and transfer can equally be the basis for a `Tree`'s `Trunk`.
    
    Excitingly, this philosophy couples incredibly well with Rust's type
    system. These ideas form a relationship that could bring interesting results.

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,25 @@ pub mod read;
 pub mod write;
 pub mod plan;
 pub mod utils;
+
+/// A [`Tree`] that has been serialized to a Json string.
+pub type TreeJson = String;
+
+/// A universally unique identifier that is pinned to the [`Tree`].
+///
+/// Generally, not displayed to the user.
+pub type TreeUuid = String;
+
+/// The leftmost [`Group`] of the [`Tree`].
+pub type Trunk = String;
+
+/// A pair of words, connected by a single `-`.
+///
+/// # Examples
+///
+/// * `endearing-shredder`.
+/// * `flirty-bucket`.
+pub type Group = String;
+
+/// One of more [`Group`]s that are connected to describe an item's path.
+pub type Tree = String;
