42adde6c5e6d9f05a1fc54476b40d41f006db69e
Chris Perry <christopher.perry410@gmail.com>
2023-02-25 13:13:31 -0500

    Feat: retrieve trunk names from directory path
    Implementation for this feature is a simple `read_dir` call, converting
    the `OsStr` into an owned `String`, and finally filtering out anything
    that is not a Json file.
    
    Down the line, there will likely be reasoning to include other
    serialized Json files alongside entries. An extra call to filter can be
    used with a fallible conversion to yank anything out when needed.

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,9 @@
 //! Internal utilities for convenience.
+use std::{collections::HashSet, path::Path};
+use anyhow::Result;
 use parity_wordlist::random_phrase;
 
+use crate::Trunk;
 /// Generate a phrase with two parity words and a Uuid, delimited by a '-'.
 ///
 /// See [`gen_pw`] for a version without the Uuid.
@@ -19,3 +22,18 @@ pub fn gen_pw() -> String {
     let word2 = random_phrase(1);
     format!("{word}-{word2}")
 }
+
+type TrunkFileName = String;
+pub fn gather_records<P>(path: P) -> Result<Vec<TrunkFileName>>
+where
+    P: AsRef<Path>,
+{
+    let entries = std::fs::read_dir(path)?.map(|res| res.map(|e| e.file_name()));
+    let strings = entries
+        .flat_map(|os_str| os_str.map(|x| x.into_string()))
+        .flatten()
+        .filter(|x| x.ends_with(".json"))
+        .collect::<Vec<_>>();
+    Ok(strings)
+}
+
